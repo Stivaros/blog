39067dd29fb0de3f59a2a8c3569839ad89cb0fc5
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2020-04-10 21:23:48 +0100

    :wrench: Update config file with my preferences

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Efstathios Stivaros`,
+      summary: `a full-stack developer (predominantly Ruby on Rails) from London.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `My blog, mainly about code and other nerdy stuff`,
+    siteUrl: `http://blog.stivaros.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `eStivaros`,
     },
   },
   plugins: [
@@ -53,20 +53,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-44876112-9`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Efstathios Stivaros | Blog`,
+        short_name: `ES' Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#8bc34a`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
