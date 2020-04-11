cf261db973857c95ad8439bd89b8282718f6d6d7
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2020-04-11 09:50:27 +0100

    :lipstick: Update typography styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,34 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import fairyGatesTheme from "typography-theme-fairy-gates"
 
-Wordpress2016.overrideThemeStyles = () => {
+fairyGatesTheme.overrideThemeStyles = () => {
+  const linkColor = `#8bc34a`;
+  const linkDarkShade = `#689f38`;
+  const linkLightShade = `#dcedc8`;
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
+    "a": {
+      color: linkColor,
+      textShadow: `none`,
+      backgroundImage: `none`
+    },
+    "a:hover": {
+      color: linkDarkShade,
+      transition: `all 0.4s`
+    },
+    "blockquote": {
+      backgroundColor: linkLightShade,
+      borderLeftColor: linkColor,
+      padding: `0.5em`,
+    }
   }
 }
 
-delete Wordpress2016.googleFonts
+delete fairyGatesTheme.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(fairyGatesTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
