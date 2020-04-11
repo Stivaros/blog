e525be73cd09d8a3acd0b471d492b1ba87bfceef
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2020-04-11 10:56:11 +0100

    :lipstick: Update Bio component with social media icons

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,6 +8,9 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 import { rhythm } from "../utils/typography"
 
@@ -29,6 +32,7 @@ const Bio = () => {
           }
           social {
             twitter
+            linkedIn
           }
         }
       }
@@ -40,7 +44,6 @@ const Bio = () => {
     <div
       style={{
         display: `flex`,
-        marginBottom: rhythm(2.5),
       }}
     >
       <Image
@@ -57,11 +60,27 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
+        I am <strong>{author.name}</strong>, {author.summary}<br /><br />
+        Find me online
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        <span
+          style={{
+            backgroundColor: `#dcedc8`,
+            color: `#689f38`,
+            padding: `0.4em 0.25em 0.25em`,
+            borderRadius: `0.25em`
+          }}
+        >
+          <a href={`http://stivaros.com/`} style={{margin: `0 0.25em`}}>
+            <FontAwesomeIcon icon={faHome} />
+          </a>
+          <a href={`https://twitter.com/${social.twitter}`} style={{margin: `0 0.25em`}}>
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href={`https://linkedin.com/in/${social.linkedIn}`} style={{margin: `0 0.25em`}}>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </span>
       </p>
     </div>
   )
