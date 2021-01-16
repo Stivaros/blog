934f82d09cd3cff44cd2178b81b9b7a071f87c11
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-01-16 12:32:51 +0000

    ➖ Remove deprecated google font preloader

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,21 +14,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Work Sans`,
-            variants: [`600`],
-          },
-          {
-            family: `Quattrocento Sans`,
-            variants: [`400`, `400i`, `700`]
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
