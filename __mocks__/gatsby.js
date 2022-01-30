cc9363972660b51fb6bb75bf947842368e31d73b
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2022-01-30 21:48:29 +0200

    🤡 Add mock for Gatsby

---
+++ __mocks__/gatsby.js
@@ -0,0 +1,26 @@
+const React = require("react")
+const Gatsby = jest.requireActual("gatsby")
+
+module.exports = {
+  ...gatsby,
+  graphql: jest.fn(),
+  Link: jest.fn().mockImplementation(
+    ({
+      activeClassName,
+      activeStyle,
+      getProps,
+      innerRef,
+      partiallyActive,
+      ref,
+      replace,
+      to,
+      ...rest
+    }) =>
+      React.createElement("a", {
+        ...rest,
+        href: to,
+      })
+  ),
+  StaticQuery: jest.fn(),
+  useStaticQuery: jest.fn(),
+}
