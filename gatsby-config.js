39b7a05d66c203b12459f0fc2b3a8e5fa8a01d2e
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2022-03-01 21:54:29 +0200

    chore: upgrade to Gatsby 3.14.6
    Refs: #385, Fixes #379

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,9 +86,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-percy`
   ],
